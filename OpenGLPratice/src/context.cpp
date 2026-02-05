c6d2818f081c005f8038a858b904c722ef4512af
MomDra <yem9087@gmail.com>
2026-02-05 19:31:50 +0900

    vertexColor GLSL

--- OpenGLPratice/src/context.cpp
+++ OpenGLPratice/src/context.cpp
@@ -14,15 +14,24 @@ void Context::Render()
     glClear(GL_COLOR_BUFFER_BIT);
     program->Use();
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
+
+    // static float time{0.0f};
+    // float t{sinf(time) * 0.5f * 0.5f};
+    // auto loc{glGetUniformLocation(program->Get(), "color")};
+    // program->Use();
+    // glUniform4f(loc, t * t, 2.0f * t * (1.0f - t), (1.0f - t) * (1.0f - t), 1.0f);
+    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
+
+    // time += 0.00016f;
 }
 
 bool Context::Init()
 {
-    std::array<float, 12> vertices{
-        0.5f, 0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f, 0.5f, 0.0f};
+    std::array<float, 24> vertices{
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f};
 
     std::array<uint16_t, 6> indices{
         0, 1, 3,
@@ -31,12 +40,13 @@ bool Context::Init()
     vertexLayout = VertexLayout::Create();
     vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices.data(), sizeof(float) * vertices.size());
 
-    vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3);
 
     indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices.data(), sizeof(uint16_t) * indices.size());
 
-    ShaderPtr vertexShader{Shader::CreateFromFile("./OpenGLPratice/shader/simple.vs", GL_VERTEX_SHADER)};
-    ShaderPtr fragmentShader{Shader::CreateFromFile("./OpenGLPratice/shader/simple.fs", GL_FRAGMENT_SHADER)};
+    ShaderPtr vertexShader{Shader::CreateFromFile("./OpenGLPratice/shader/perVertexColor.vs", GL_VERTEX_SHADER)};
+    ShaderPtr fragmentShader{Shader::CreateFromFile("./OpenGLPratice/shader/perVertexColor.fs", GL_FRAGMENT_SHADER)};
 
     if (!vertexShader || !fragmentShader)
         return false;
